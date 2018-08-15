4fc77243f8b77a49b6e49a1dc8d667753e9168a4
Harry Cresswell <studio@harrycresswell.com>
2018-08-15 19:43:59 +0200

    fixed off canvas nav

--- src/js/app.js
+++ src/js/app.js
@@ -1,15 +1,16 @@
 $(document).ready(function(){
-
   /* when .Hamburger is clicked */
   $('.Hamburger').click(function(){
-
     /* add .is-active to animate */
     $(this).toggleClass('is-active');
-
-    var $body = $( 'body' ),
-        $page = $( '.Main' )
-
-        /* add class .is-animating to body */
-        $body.toggleClass( 'is-animating' )
+    /* add .is-visible to show nav */
+    $( '.Navigation' ).toggleClass('is-visible');
+    /* when Nav link is clicked */
+    $('.Header-link').click(function(){
+      /* hide nav */
+      $('.Navigation').removeClass('is-visible');
+      /* and remove active class from Hamburger */
+      $('.Hamburger').removeClass('is-active');
+    });
   });
 });
